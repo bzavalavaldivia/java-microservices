0c392a5bf3b30e216025f53f674e9169b83dfbe5
Bryan Zavala <bzavalavaldivia@gmail.com>
2023-07-01 17:27:09 -0500

    feat: eureka server

--- ms-motorcycles/src/main/java/com/ms/motorcycles/MsMotorcyclesApplication.java
+++ ms-motorcycles/src/main/java/com/ms/motorcycles/MsMotorcyclesApplication.java
@@ -2,8 +2,10 @@ package com.ms.motorcycles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsMotorcyclesApplication {
 
 	public static void main(String[] args) {
